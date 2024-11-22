d1104b55bb8e4caacd75246129aabc815031bbd1
Imaad Uddin <imaaduddin27@gmail.com>
2024-11-22 00:27:44 -0600

    variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,13 @@
-import { apiKey, abc } from "./utils.js";
+// import { apiKey, abc } from "./utils.js";
 // import apiKey from "./utils.js"
 
-console.log(apiKey);
-console.log(abc);
+// console.log(apiKey);
+// console.log(abc);
+
+
+let userMessage = "Hello!";
+const anotherMessage = "Hi!!";
+
+console.log(userMessage);
+
+
